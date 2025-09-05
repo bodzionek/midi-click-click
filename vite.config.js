a4c7660ebacd315c86e3f20244a9f02105eda67f
Bodzionek <pebe83@gmail.com>
2025-09-05 17:27:17 +0200

    Added deployment script

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bodzionek/midi-click-click/
+  base: "/midi-click-click/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
